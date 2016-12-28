82d9a122fe58f3f9a6ac0358b5e6c2ec868256c6
Marius Landwehr <marius.landwehr@gmail.com>
2016-12-28 23:29:12 +0100

    fetching is only done when data is changed

--- DatabasePattern/AthletsTVC.swift
+++ DatabasePattern/AthletsTVC.swift
@@ -13,32 +13,38 @@ import RealmSwift
 class AthletsTVC: UITableViewController {
     
     let dataSource = AthletsCoreDataDataSource() // AthletsRealmDataSource()
+    var athlets = [Athlet]()
+    
+    func fetchData() {
+        athlets = dataSource.all()
+    }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        fetchData()
         tableView.reloadData()
     }
-
+    
     // MARK: - Table view data source
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return dataSource.getAll().count > 0 ? 1 : 0
+        return athlets.count > 0 ? 1 : 0
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return dataSource.getAll().count
+        return athlets.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "athletCell", for: indexPath)
-
+        
         // Configure the cell...
-        cell.textLabel?.text = dataSource.getAll()[indexPath.row].name
-
+        cell.textLabel?.text = athlets[indexPath.row].name
+        
         return cell
     }
-
+    
     // MARK: - Actions
     
     @IBAction func addBtnTouched(_ sender: UIBarButtonItem) {
@@ -51,36 +57,38 @@ class AthletsTVC: UITableViewController {
         alertController.addAction(UIAlertAction(title: "Add", style: .default) { _ in
             guard let name = alertTextField.text , !name.isEmpty else { return }
             self.dataSource.insert(item: Athlet(withId: UUID().uuidString, name: name))
-            self.tableView.reloadData()
+            self.fetchData()
+            let newRowIndexPath = IndexPath(row: self.athlets.count-1, section: 0)
+            self.tableView.insertRows(at: [newRowIndexPath], with: .automatic)
         })
         present(alertController, animated: true, completion: nil)
     }
     
-
+    
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-
-
+    
+    
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
             
-           let athlet = dataSource.getAll()[indexPath.row]
-           dataSource.delete(item: athlet)
-
+            let athlet = athlets[indexPath.row]
+            dataSource.delete(item: athlet)
+            athlets.remove(at: indexPath.row)
             
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
     
-
-
+    
+    
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
@@ -88,10 +96,10 @@ class AthletsTVC: UITableViewController {
         
         if segue.identifier == "showAthlet" {
             if let athletVC = segue.destination as? AthletVC, let index = tableView.indexPathForSelectedRow?.row {
-                athletVC.athlet = dataSource.getAll()[index]
+                athletVC.athlet = athlets[index]
             }
-          
+            
         }
     }
-
+    
 }
